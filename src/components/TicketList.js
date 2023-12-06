1446ff3b6d1f47301b7abef9f2b8cccaaa69fc11
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-06 15:32:18 -0800

    Update Ticket and TicketList to accept new PropTypes

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -2,22 +2,26 @@ import React from "react";
 import Ticket from "./Ticket";
 import PropTypes from "prop-types";
 
-function TicketList(props){
+function TicketList(props) {
   return (
     <React.Fragment>
-      <hr/>
-        {props.ticketList.map((ticket, index) =>
-          <Ticket names={ticket.names}
-            location={ticket.location}
-            issue={ticket.issue}
-            key={index}/>
-        )}
+      <hr />
+      {props.ticketList.map((ticket) =>
+        <Ticket 
+          whenTicketClicked = { props.onTicketSelection }
+          names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          id = {ticket.id}
+          key={ticket.id} />
+      )}
     </React.Fragment>
   );
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.array
+  ticketList: PropTypes.array,
+  onTicketSelection: PropTypes.func
 };
 
 export default TicketList;
