dca72c078118fdfaa9b1c40e4bf2d21587dc7f52
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-06 20:07:50 -0800

    Update ReusableForm call to send handler for Editing

--- src/components/EditTicketForm.js
+++ src/components/EditTicketForm.js
@@ -3,9 +3,15 @@ import ReusableForm from "./ReusableForm";
 import PropTypes from "prop-types";
 
 function EditTicketForm (props) {
+  const { ticket } = props;
+  function handleEditTicketFormSubmission(event) {
+    event.preventDefault();
+    props.onEditTicket({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: ticket.id});
+  }
   return (
     <React.Fragment>
       <ReusableForm 
+        formSubmissionHandler={handleEditTicketFormSubmission}
         buttonText="Update Ticket" />
     </React.Fragment>
   );
