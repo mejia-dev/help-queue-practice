5a2c735a7552364c3c70213251bb02ee546357d2
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-09 17:27:02 -0800

    Modify TicketList to use the object ticketList instead of array

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -2,25 +2,26 @@ import React from "react";
 import Ticket from "./Ticket";
 import PropTypes from "prop-types";
 
-function TicketList(props) {
+function TicketList(props){
   return (
     <React.Fragment>
       <hr />
-      {props.ticketList.map((ticket) =>
-        <Ticket 
+      {Object.values(props.ticketList).map((ticket) =>
+        <Ticket
           whenTicketClicked = { props.onTicketSelection }
           names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
-          id = {ticket.id}
-          key={ticket.id} />
+          id={ticket.id}
+          key={ticket.id}/>
       )}
     </React.Fragment>
   );
 }
 
+
 TicketList.propTypes = {
-  ticketList: PropTypes.array,
+  ticketList: PropTypes.object,
   onTicketSelection: PropTypes.func
 };
 
