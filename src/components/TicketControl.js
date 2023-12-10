5cf1c7f7a9c9d66c6ab98bb11f82159263834b23
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-09 17:25:09 -0800

    Refactor TicketControl to use Redux

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -3,6 +3,7 @@ import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
 import TicketDetail from './TicketDetail';
 import EditTicketForm from './EditTicketForm';
+import PropTypes from "prop-types";
 
 class TicketControl extends React.Component {
 
@@ -45,7 +46,7 @@ class TicketControl extends React.Component {
   }
 
   handleChangingSelectedTicket = (id) => {
-    const selectedTicket = this.state.mainTicketList.filter(ticket => ticket.id === id)[0];
+    const selectedTicket = this.props.mainTicketList[id];
     this.setState({selectedTicket: selectedTicket});
   }
 
@@ -101,7 +102,7 @@ class TicketControl extends React.Component {
       buttonText = "Return to Ticket List";
     } else {
       currentlyVisibleState = <TicketList 
-        ticketList={this.state.mainTicketList}
+        ticketList={this.props.mainTicketList}
         onTicketSelection={this.handleChangingSelectedTicket}
       />;
       buttonText = "Add Ticket";
@@ -116,6 +117,16 @@ class TicketControl extends React.Component {
 
 }
 
-TicketControl = connect()(TicketControl);
+TicketControl.propTypes = {
+  mainTicketList: PropTypes.object
+};
+
+const mapStateToProps = state => {
+  return {
+    mainTicketList: state
+  }
+}
+
+TicketControl = connect(mapStateToProps)(TicketControl);
 
 export default TicketControl;
