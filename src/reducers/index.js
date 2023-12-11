3a578ff52d319d4f789aac89d5ad6175bd3df727
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-11 13:33:13 -0800

    Import rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import formVisibleReducer from "./form-visible-reducer";
 import ticketListReducer from "./ticket-list-reducer";
 import { combineReducers } from "redux";
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
