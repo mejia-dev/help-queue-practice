6503fbad36a8268df8db98b0819c8a86b17d29c4
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-13 08:55:43 -0800

    Handle time on ticket object and EditTicketForm

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -4,9 +4,10 @@ import PropTypes from "prop-types";
 function Ticket(props){
   return (
     <React.Fragment>
-      <div onClick = {() => props.whenTicketClicked(props.id)}>
+      <div onClick = {()=> props.whenTicketClicked(props.id)}>
         <h3>{props.location} - {props.names}</h3>
         <p><em>{props.issue}</em></p>
+        <p><em>{props.formattedWaitTime}</em></p>
         <hr/>
       </div>
     </React.Fragment>
@@ -18,7 +19,8 @@ Ticket.propTypes = {
   location: PropTypes.string.isRequired,
   issue: PropTypes.string,
   id: PropTypes.string,
-  whenTicketClicked: PropTypes.func
+  whenTicketClicked: PropTypes.func,
+  formattedWaitTime: PropTypes.string
 };
 
 export default Ticket;
